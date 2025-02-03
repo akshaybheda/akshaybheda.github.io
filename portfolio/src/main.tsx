942274e07760df15d22dcd96b94cf693dac2d9d3
Akshay Bheda <Akshay.Bheda@bentley.com>
2025-02-03 14:55:58 -0500

    added react router

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Homepage from "./Homepage.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} >
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);

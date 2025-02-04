505594fc088d7f73297a02921ac0088dec9a3e68
Akshay Bheda <Akshay.Bheda@bentley.com>
2025-02-03 21:45:50 -0500

    added materialui

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,15 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Homepage from "./components/Homepage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+const theme  = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} >
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme} defaultMode="dark" >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,26 +1,10 @@
 // Navbar.tsx
-import { Navbar, Nav, Button } from "react-bootstrap";
 
 export default function AppNavbar() {
-  const toggleDarkMode = () => {
-    if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
-      document.documentElement.setAttribute("data-bs-theme", "light");
-    } else {
-      document.documentElement.setAttribute("data-bs-theme", "dark");
-    }
-  };
+
   return (
-    <Navbar>
-      <Nav className="ms-auto">
-        <Button className="btn btn-dark shadow" onClick={toggleDarkMode}>
-          Toggle Dark Mode
-        </Button>
-        <Nav.Link className="" href="">
-          Home
-        </Nav.Link>
-        <Nav.Link href="projects">Projects</Nav.Link>
-        <Nav.Link href="contact">Contact</Nav.Link>
-      </Nav>
-    </Navbar>
+ <div>
+    
+ </div>
   );
 }

347e55b881df0e3d9dae3f9fc543f33a3eac6781
Akshay Bheda <Akshay.Bheda@bentley.com>
2025-02-09 17:32:36 -0500

    added admin portal

--- portfolio/src/components/Projects/data/ProjectsData.ts
+++ portfolio/src/components/Projects/data/ProjectsData.ts
@@ -9,11 +9,11 @@ export const ProjectsData: ProjectInfo[] = [
     AdditionalText3: "Interesting thing to know about this service, During the first deployment of the URL Discovery Service, performance lagged behind our benchmarks. After deep investigation, I traced the issue to the SoapCore NuGet package—essential for SOAP support in .NET Core. I fixed the problem in its codebase, upgraded our version, and successfully deployed the service at peak efficiency. This not only resolved a critical bottleneck but also marked my first impactful contribution to open source.",
   },
   {
-    Title: "Project 2",
-    Description: "Description 2",
-    AdditionalText1: "Additional Text 1",
-    AdditionalText2: "Additional Text 2",
-    AdditionalText3: "Additional Text 3",
+    Title: "Bentley Admin Portal",
+    Description: "Leading the development of a new, unified admin portal to streamline user management, reporting, and license assignments. Architected the system with future scalability in mind, ensuring a seamless transition to microservices when needed. Implemented Onion Architecture in C# for a clean, maintainable backend and adopted the Bulletproof React structure with TypeScript for a robust, scalable frontend—prioritizing performance, security, and long-term adaptability.",
+    AdditionalText1: "To ensure a future-proof architecture, I conducted extensive research on clean code principles, micro-frontend, and microservices.",
+    AdditionalText2: "While exploring new technologies is valuable, practicality is key—we must balance innovation with development efficiency. It's crucial to weigh the benefits of emerging frameworks against the time required for implementation and maintenance. My approach ensures we adopt the right mix of proven solutions and modern advancements, optimizing both long-term scalability and immediate productivity.",
+    AdditionalText3: "",
   },
   {
     Title: "Project 3",
